e7d7efd6710ccc4f9358a80386b616472557e90d
Ioan CHIRIAC <i.chiriac@yahoo.com>
2017-01-25 22:25:47 +0100

    impl the main api

--- src/index.js
+++ src/index.js
@@ -5,26 +5,121 @@
  */
 'use strict';
 
-var Parser = require('php-parser');
-var Transpiler = require('php-transpiler');
+var extend = require('extend');
+var Core = require('./core');
+var Stream = require('./stream');
+var Context = require('./context/index');
+var phpArray = require('./array');
 
 
 /**
  * @constructor Runtime
  */
 var Runtime = function(options) {
+  // the core engine
+  this.core = new Core(this);
+  // the context
+  this.context = new Context();
+  // custom / hack options
   if (options) {
-    for(var k in options) {
-      this[k] = options;
+    /**
+    Sample :
+{
+  core: {
+    config: {
+      // PRODUCTION SETUP :
+      error_log: 'syslog',
+      display_errors: false,
+      display_startup_errors: false,
+      // CUSTOM EXTENSIONS :
+      extension: ['glayzzle-common', 'glayzzle-mysqlnd', 'glayzzle-gd2']
     }
   }
-  if (!this.parser) {
-    this.parser = new Parser({
-      parser: {
-
-      }
-    });
+}
+    **/
+    extend(true, this, options);
   }
+  /** initialize streams **/
+  this.stdout = new Stream();
+  this.stderr = new Stream();
+  /** registers core functions **/
+  require('./ext/constants')(this);
+  require('./ext/errors')(this);
+  /** @todo starts extensions **/
+};
+
+/**
+ * Run an eval over the specified PHP code
+ */
+Runtime.prototype.eval = function(code) {
+  return this.core.eval(code)(this);
+};
+
+/**
+ * Creates a new array instance
+ */
+Runtime.prototype.array = function() {
+  return new phpArray(arguments);
+};
+
+/**
+ * Run an inclusion over the specified filename
+ */
+Runtime.prototype.module = function(filename, once, required) {
+  if (once) {
+    // resolves filename before check
+    filename = this.core.resolveFilename(filename);
+    if (filename in this.context.includes) {
+      return null;
+    }
+  }
+  var result;
+  // try to run the include
+  try {
+    result = this.core.include(filename);
+    this.context.includes[result.filename] = true;
+  } catch(e) {
+    // generate an error
+    var fnName = required ? 'require' : 'include';
+    if (once) {
+      fnName += '_once';
+    }
+    this.trigger_error(
+      fnName + '(): Failed opening \'' + filename + '\' for inclusion (' +
+      'include_path=\'' + this.core.config.include_path.join(':') + '\')',
+      this.constant(required ? 'E_ERROR' : 'E_WARNING')
+    );
+    return null;
+  }
+  return result.fn(this);
+};
+
+/**
+ * run a eval over the specified PHP code
+ */
+Runtime.prototype.include = function(filename) {
+  return this.module(filename, false, false);
+};
+
+/**
+ * includes the specified script only one time
+ */
+Runtime.prototype.include_once = function(filename) {
+  return this.module(filename, true, false);
+};
+
+/**
+ * requires the specified script and throws an error if its not found
+ */
+Runtime.prototype.require = function(filename) {
+  return this.module(filename, false, true);
+};
+
+/**
+ * require_once
+ */
+Runtime.prototype.require_once = function(filename) {
+  return this.module(filename, true, true);
 };
 
 module.exports = Runtime;
